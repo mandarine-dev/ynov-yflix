85bb24aa5d2e1f27bf5766491d7656d50474542d
Sushizi <pierre.cicuto@gmail.com>
2018-11-24 16:19:48 +0100

    UserProfile AngularMaterial 5%

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,16 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthentificationService } from '@app/core/services/authentification/authentification.service';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
+
 export class UserProfileComponent implements OnInit {
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+   matcher = new MyErrorStateMatcher();
 
   constructor(public auth: AuthentificationService) { }
 
   ngOnInit() {
   }
 
+}
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+export interface Culture {
+  value: string;
+  viewValue: string;
+}
+
+export class SelectCultureValue {
+  cultures: Culture[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
 }
